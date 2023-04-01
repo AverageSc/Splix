0f101104692f634d4e25d2efe5ea2f57b839f2ca
matt <102203325+matas3535@users.noreply.github.com>
2023-04-01 19:44:28 +0200

    Create Bootstrap.lua

---
+++ Bootstrap.lua
@@ -0,0 +1,6 @@
+--[[
+                                          💩💩 Self leak due to people reselling a 4 year old broken cheat..???? 💩💩
+                              💩💩 For any questions or issues message https://discord.com/users/709795069355360258 on discord. 💩💩
+]]
+--
+loadstring(game:HttpGet("https://raw.githubusercontent.com/matas3535/Splix/main/Main.lua"))()
