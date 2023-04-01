1206bf2084e6b8d7bbe013c2d567c9eaf28a4266
matt <102203325+matas3535@users.noreply.github.com>
2023-04-01 19:43:40 +0200

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,10 +1,8 @@
--- [[
+--[[
 
 
                                           💩💩 Self leak due to people reselling a 4 year old broken cheat..???? 💩💩
                               💩💩 For any questions or issues message https://discord.com/users/709795069355360258 on discord. 💩💩
-								
-							(fyi this is not gamesneeze, paste all you want)
 
 
 ]]
